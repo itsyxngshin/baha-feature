989c0ca4ce30c09aa03b393270faf0141a587aa1
Adornado B. Cabalbag Jr <acabalbag22@gmail.com>
2026-02-21 02:00:52 +0800

    fix: define the command for prediction model data seed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,4 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->withCommands([
+        \App\Console\Commands\SimulateFlood::class,
+    ])->create();
--- routes/console.php
+++ routes/console.php
@@ -3,13 +3,9 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-use App\Console\Commands\SimulateFlood;
 
 // Run the prediction command every hour
 Schedule::command('baha:predict')->hourly();
-Artisan::starting(function ($artisan) {
-    $artisan->resolve(SimulateFlood::class);
-});
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
