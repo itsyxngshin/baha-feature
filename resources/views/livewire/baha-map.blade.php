28d74e3ec748ee1b3470455a3d0fb3d2fbb03801
Adornado B. Cabalbag Jr <acabalbag22@gmail.com>
2026-02-21 00:12:22 +0800

    fix: implement the data display

--- resources/views/livewire/baha-map.blade.php
+++ resources/views/livewire/baha-map.blade.php
@@ -1,6 +1,3 @@
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
-
 <div class="relative h-screen w-full overflow-hidden bg-gray-100 font-sans"
      x-data="{
         map: null,
